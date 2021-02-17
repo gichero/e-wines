523e65fdb5a99273be7076e4c1fddcd77f55021a
Amos Gichero <amosgichero@gmail.com>
2021-02-16 23:02:52 -0500

    connect to database and define models

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -24,5 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
 );
