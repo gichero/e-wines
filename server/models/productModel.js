1ed190eb5c3338d4f516a3423444523f94f1eb70
Amos Gichero <amosgichero@gmail.com>
2021-02-18 02:59:04 -0500

    added error handling middleware

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,26 +1,7 @@
 /** @format */
 
 import mongoose from "mongoose";
-
-const reviewSchema = mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		rating: {
-			type: Number,
-			required: true,
-		},
-		comment: {
-			type: String,
-			required: true,
-		},
-	},
-	{
-		timestamps: true,
-	}
-);
+import reviewSchema from "./reviewModel.js";
 
 const productSchema = mongoose.Schema(
 	{
