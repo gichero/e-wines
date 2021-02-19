2c5c2c0f515731b92165e45fcf616780e64cb800
Amos Gichero <amosgichero@gmail.com>
2021-02-19 13:13:10 -0500

    add images and install and configure redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./bootstrap.min.css";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 
 const app = (
-	<React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>
+	</Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 connectDB();
+
 const app = express();
 
 app.get("/", (req, res) => {
