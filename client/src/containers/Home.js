4494b10be68f03faaad0768f448965762def1d3c
Amos Gichero <amosgichero@gmail.com>
2021-02-21 23:36:15 -0500

    react redux wired with first action

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,31 +1,38 @@
 /** @format */
 
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product/Product";
-import axios from "axios";
+import { listProducts } from "../actions/productActions";
 
 const Home = () => {
-	const [products, setProducts] = useState([]);
+	const dispatch = useDispatch();
+
+	const productList = useSelector((state) => state.productList);
+
+	const { loading, products, error } = productList;
 
 	useEffect(() => {
-		const getProducts = async () => {
-			const res = await axios.get("/api/products");
-			setProducts(res.data);
-		};
-		getProducts();
-	}, []);
+		dispatch(listProducts());
+	}, [dispatch]);
 
 	return (
 		<>
 			<h1>products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<h3>{error}</h3>
+			) : (
+				<Row>
+					{products.map((product) => (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</>
 	);
 };
--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducer } from "./reducers/productReducers";
 
-const reducer = combineReducers({});
+const reducers = combineReducers({ productList: productListReducer });
 const initialState = {};
 const middleware = [thunk];
+
 const store = createStore(
-	reducer,
+	reducers,
 	initialState,
 	composeWithDevTools(applyMiddleware(...middleware))
 );
