c6b19ff7ada373636158f8a004456473395abeeb
Amos Gichero <amosgichero@gmail.com>
2021-03-03 01:29:04 -0500

    register new user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,11 @@
 /** @format */
 
 import express from "express";
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+	authUser,
+	getUserProfile,
+	registerUser,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -9,5 +13,7 @@ const router = express.Router();
 router.post("/login", authUser);
 
 router.route("/profile").get(protect, getUserProfile);
+
+router.route("/").post(registerUser);
 
 export default router;
