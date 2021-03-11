fa6e32e7c2a9d0375608b1207a7a917492331abb
Amos Gichero <amosgichero@gmail.com>
2021-03-11 02:27:46 -0500

    placeOrderPage client side complete

--- client/src/pages/PlaceOrderPage.js
+++ client/src/pages/PlaceOrderPage.js
@@ -10,9 +10,29 @@ import CheckoutSteps from "../components/CheckoutSteps/CheckoutSteps";
 const PlaceOrderPage = () => {
 	const cart = useSelector((state) => state.cart);
 
+	//Calculate prices
+	const addDecimals = (num) => {
+		return (Math.round(num * 100) / 100).toFixed(2);
+	};
+
+	cart.itemsPrice = addDecimals(
+		cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+	);
+
+	cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 25);
+
+	cart.taxPrice = addDecimals(Number(0.15 * cart.itemsPrice).toFixed(2));
+
+	cart.totalPrice = (
+		Number(cart.itemsPrice) +
+		Number(cart.shippingPrice) +
+		Number(cart.taxPrice)
+	).toFixed(2);
+
 	const placeOrderHandler = () => {
 		console.log("order");
 	};
+
 	return (
 		<>
 			<CheckoutSteps step1 step2 step3 step4 />
