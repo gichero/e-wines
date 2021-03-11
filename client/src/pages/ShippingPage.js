28f67b7fc6714788c1533f992dc192dfc613bff4
Amos Gichero <amosgichero@gmail.com>
2021-03-10 23:02:28 -0500

    checkoutSteps component added

--- client/src/pages/ShippingPage.js
+++ client/src/pages/ShippingPage.js
@@ -1,13 +1,13 @@
 /** @format */
 
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message/Message";
 import Loader from "../components/Loader/Loader";
 import FormContainer from "../components/FormContainer/FormContainer";
 import { saveShippingAddress } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps/CheckoutSteps";
 
 const ShippingPage = ({ history }) => {
 	const cart = useSelector((state) => state.cart);
@@ -28,6 +28,7 @@ const ShippingPage = ({ history }) => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 
 			<Form onSubmit={submitHandler}>
