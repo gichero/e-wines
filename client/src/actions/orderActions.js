727c3d6e5b869c21f9989bbb22cbf7283761bfec
Amos Gichero <amosgichero@gmail.com>
2021-03-15 14:46:06 -0400

    order route, constroller, actiontypes, reducer complete

---
+++ client/src/actions/orderActions.js
@@ -0,0 +1,25 @@
+/** @format */
+
+import axios from "axios";
+import {
+	ORDER_CREATE_REQUEST,
+	ORDER_CREATE_SUCCESS,
+	ORDER_CREATE_FAIL,
+} from "../actions/actionTypes";
+
+export const createOrder = (order) => async (dispatch, getState) => {
+	try {
+		dispatch({ type: ORDER_CREATE_REQUEST });
+
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = {
+			headers: {
+				"Content-type": "application/json",
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+	} catch (error) {}
+};
