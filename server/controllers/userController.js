e18129a0f9916997a08ff38050b5f3bb681b990b
Amos Gichero <amosgichero@gmail.com>
2021-03-29 11:30:28 -0400

    admin to edit user, route and controller complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -134,3 +134,45 @@ export const deleteUser = asyncHandler(async (req, res) => {
 		throw new Error("User not found");
 	}
 });
+
+// @desc get user
+// @route GET /api/users/:id
+// @access Private/admin
+
+export const getUserById = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.params.id).select("-password");
+	if (user) {
+		res.json(user);
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+});
+
+// @desc update user
+// @route PUT /api/users/:id
+// @access Private/admin
+
+export const updateUser = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.params.id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+
+		user.email = req.body.email || user.email;
+
+		user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+		const updatedUser = await user.save();
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+		});
+	} else {
+		res.status(404);
+		throw new Error("User Not Found");
+	}
+});
