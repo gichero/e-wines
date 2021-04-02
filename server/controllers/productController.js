69edd62dd64962634fc6ac5dc0d0c0c664df2918
Amos Gichero <amosgichero@gmail.com>
2021-04-02 00:50:48 -0400

    create product controller and route complete

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -26,7 +26,7 @@ export const getProductById = asyncHandler(async (req, res) => {
 
 // @route DELETE /api/products/:id
 // @desc delete a product
-// @access public
+// @access Private / Admin
 export const deleteProduct = asyncHandler(async (req, res) => {
 	const product = await Product.findById(req.params.id);
 
@@ -35,5 +35,58 @@ export const deleteProduct = asyncHandler(async (req, res) => {
 		res.json({ message: "product removed" });
 	} else {
 		res.status(404).json({ message: "product not found" });
+	}
+});
+
+// @route POST /api/products
+// @desc create a product
+// @access Private / Admin
+export const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: "Sample name",
+		price: 0,
+		user: req.user._id,
+		image: "/images/sample.jpg",
+		brand: "Sample brand",
+		category: "Sample category",
+		countInStock: 0,
+		numReviews: 0,
+		description: "Sample description",
+	});
+
+	const createdProduct = await product.save();
+	res.status(201).json(createdProduct);
+});
+
+// @route PUT /api/products/:id
+// @desc update a product
+// @access Private / Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+	const {
+		name,
+		price,
+		description,
+		image,
+		brand,
+		category,
+		countInStock,
+	} = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.description = description;
+		product.image = image;
+		product.brand = brand;
+		product.category = category;
+		product.countInStock = countInStock;
+
+		const updatedProduct = await product.save();
+		res.status(201).json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error("Product not found");
 	}
 });
